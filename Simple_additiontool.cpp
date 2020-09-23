10cf7ac0f0f13b2a0991e1b4ce2d4e08c5ced352
Dhruv Singh Rajput <71124045+DhruvSinghRajput@users.noreply.github.com>
2020-09-23 19:31:04 +0530

    Create Simple_additiontool.cpp

---
+++ Simple_additiontool.cpp
@@ -0,0 +1,53 @@
+/* Hello Viewer , This is a simple addition tool which can add upto 5 numbers
+   Thankyou  */
+
+
+
+
+
+
+
+
+#include <iostream>
+
+using namespace std  ;
+
+int main()
+
+{ 
+
+    int num1 , num2 , num3 , num4 , num5 ;
+
+    cout<<"Enter first number "  ;
+    cin>>num1  ;
+    
+    cout<<"\nYour sum = "   ;
+    cout<<num1 ;
+    
+    cout<<"\nEnter second number "  ;
+    cin>>num2  ;
+    
+    cout<<"\nYour sum = "   ;
+    cout<<num1+num2  ;
+    
+    cout<<"\nEnter third number "   ;
+    cin>>num3  ;
+     
+    
+    cout<<"\nYour sum = "   ;
+    cout<<num1+num2+num3  ;
+    
+    cout<<"\nEnter forth number "   ;
+    cin>>num4  ;
+    
+    cout<<"\nYour sum = "   ;
+    cout<<num1+num2+num3+num4 ;
+    
+    cout<<"\nEnter fifth number "  ;
+    cin>>num5  ;
+    
+    cout<<"\nYour final sum is = "  ;
+    cout<<num1+num2+num3+num4+num5  ;
+    
+    return 0  ;   }
+    
