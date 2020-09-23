5718214841d5ea5788e4c927e8620a2e28c2833f
Dhruv Singh Rajput <71124045+DhruvSinghRajput@users.noreply.github.com>
2020-09-23 20:34:55 +0530

    Create simpleinterestcalculator.cpp

---
+++ simpleinterestcalculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{ int a , b , c , d = 100;
+
+  cout<<"Enter Principal Amount = " ;
+  cin>>a ;
+  
+  
+  cout<<"\nEnter Rate Of Interest = " ;
+  cin>>b ;
+  
+  cout<<"\nEnter time = " ;
+  cin>>c ;
+  
+  cout<<"\nYour Simple Interest = " ;
+  cout<<a*b*c/d ;
+  
+  return 0 ; }
