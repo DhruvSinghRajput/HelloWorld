a0730e10277d339e07df078abd0a937f1734fde0
Dhruv Singh Rajput <71124045+DhruvSinghRajput@users.noreply.github.com>
2020-09-23 20:35:49 +0530

    Create Finding_area_of_circle.cpp

---
+++ Finding_area_of_circle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std ;
+
+int main() 
+
+{  float pi = 3.14 ;
+   
+   float r ;
+   
+   cout<<"Enter The Radius Of The Circle =" ;
+   cin>>r ;
+   
+   cout<<"Desired Area of the circle =" ;
+   cout<<pi*r*r ;
+   
+   return 0 ;     }
+   
